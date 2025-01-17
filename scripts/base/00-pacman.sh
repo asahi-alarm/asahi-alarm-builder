26449c6f50f863912fde7066c38fa70dd22b0380
Jos Dehaes <jos.dehaes@gmail.com>
2025-01-17 16:05:40 +0100

    enable 5 parallel downloads

--- scripts/base/00-pacman.sh
+++ scripts/base/00-pacman.sh
@@ -1,6 +1,7 @@
 #!/bin.sh
 set -e
 
+sed -i -e 's/^#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
 sed -i -e '/\[core\]/i [asahi-alarm]\nInclude = /etc/pacman.d/mirrorlist.asahi-alarm\n' /etc/pacman.conf
 
 cp /files/mirrorlist.asahi-alarm /etc/pacman.d/mirrorlist.asahi-alarm
