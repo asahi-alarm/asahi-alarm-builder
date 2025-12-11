f88459e2060ff772e326a42883dca91bde435584
Jos Dehaes <jos.dehaes@gmail.com>
2025-12-11 11:52:14 +0100

    add grub

--- scripts/base/10-base-packages.sh
+++ scripts/base/10-base-packages.sh
@@ -7,4 +7,4 @@ touch "/boot/efi/.builder"
 
 pacman --noconfirm -R linux || true
 pacman --noconfirm -Syyu
-pacman --noconfirm -S asahi-scripts asahi-fwextract m1n1 uboot-asahi mkinitcpio grub iwd sudo vim man networkmanager
+pacman --noconfirm -S asahi-scripts asahi-fwextract m1n1 uboot-asahi mkinitcpio grub iwd sudo vim man networkmanager grub efibootmgr
