54110e3fdc3fb2ee77cb80a7a6fa498835cb5911
Jos Dehaes <jos.dehaes@gmail.com>
2026-04-13 10:53:07 +0200

    fix alpm user for download

--- scripts/base/00-pacman.sh
+++ scripts/base/00-pacman.sh
@@ -6,5 +6,9 @@ sed -i -e '/\[core\]/i [asahi-alarm]\nInclude = /etc/pacman.d/mirrorlist.asahi-a
 
 cp /files/mirrorlist.asahi-alarm /etc/pacman.d/mirrorlist.asahi-alarm
 
+# Create system users (notably `alpm`) declared in /usr/lib/sysusers.d/*.
+# Required since pacman.conf now ships DownloadUser = alpm by default.
+systemd-sysusers
+
 pacman-key --init
 pacman-key --populate
